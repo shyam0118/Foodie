35475374338c85b50d6d9e54ad837bfb587b3b9e
Shyamalendu Nayak <shyamalendu.nayak@hyscaler.com>
2024-09-05 07:51:19 +0530

    updated

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,11 +3,11 @@ import { categories } from '../data/Data'
 
 const Category = () => {
     return (
-        <div className='max-w-[1640px] px-4 py-12'>
-            <h1 className='text-orange-600 font-bold text-4xl text-center' >Top Rated Menu Items</h1>
+        <div className='max-w-[1640px] px-4 py-12 m-auto'>
+            <h1 className='text-orange-600 font-bold text-4xl text-center' >Top Rated Menu List</h1>
             <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6' >
                 {categories.map((item, index) => (
-                    <div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center' >
+                    <div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center cursor-pointer' >
                         <h2 className='font-bold sm:text-xl'>{item.name}</h2>
                         <img src={item.image} alt={item.name} className='w-20' />
 
